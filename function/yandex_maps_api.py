8b51980a0151ae2be116dbf2cf77899466aac7a4
LeadM <elpuffika@vk.com>
2023-04-25 01:12:57 +0300

    Добавлена поддержка Yandex.Maps (В дальнейшем бот сможет отправлять картинку с ближайшим путем от точки A до точки B).

---
+++ function/yandex_maps_api.py
@@ -0,0 +1,51 @@
+import requests
+import json
+
+def get_nearest_metro(lat, lng):
+    url = "http://geocode-maps.yandex.ru/1.x/"
+    params = {
+        "apikey": '40d1649f-0493-4b70-98ba-98533de7710b',
+        "geocode": f"{lng},{lat}",
+        "format": "json",
+        "kind": 'metro'
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    pos = (data["response"]["GeoObjectCollection"]["metaDataProperty"]
+    ["GeocoderResponseMetaData"]["Point"]['pos']).split()
+    try:
+        nearest_metro = data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]['name']
+        return nearest_metro
+    except Exception:
+        return 0
+
+near_metro = get_nearest_metro(55.846456, 37.562658)
+def get_coords(place):
+    url = "http://geocode-maps.yandex.ru/1.x/"
+    params = {
+        "apikey": '40d1649f-0493-4b70-98ba-98533de7710b',
+        "geocode": place,
+        "format": "json",
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    try:
+        position = (data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]['pos']).split()
+        return position
+    except Exception:
+        return 0
+
+
+def get_route_image(lat1, lng1, lat2, lng2):
+    api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
+    url = "https://static-maps.yandex.ru/1.x/?"
+    params = {
+        "ll": f"{(lng1 + lng2) / 2},{(lat1 + lat2) / 2}",
+        "size": "600,450",
+        "z": "14",
+        "l": "map",
+        "pt": f"{lng1},{lat1},pm2rdl~{lng2},{lat2},pm2gnm",
+        "apikey": api_key
+    }
+    response = requests.get(url, params=params)
+    return response.content
--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('Hello, world!')
+import requests
+import logging
+import io
+from function.yandex_maps_api import get_nearest_metro, get_route_image, get_coords
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, Bot
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    update.message.reply_text('Здравствуйте, меня зовут TBot. Я многофункциональный бот-помощник. '
+                              'Выберите необходимое действие.')
+
+
+def location(update, context):
+    message = update.message
+    current_position = (message.location.longitude, message.location.latitude)
+    metro_near = get_nearest_metro(current_position[1], current_position[0])
+    cord_metro = get_coords(metro_near)
+    responsed = get_route_image(float(current_position[1]), float(current_position[0]), float(cord_metro[1]),
+                                float(cord_metro[0]))
+
+    update.message.reply_text(f'Ближайшая станция: {metro_near}')
+
+
+
+def main():
+
+    updater = Updater('5987276262:AAETidokunbdPmBFmnB7eLFYECH_FyJZBp8', use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.location, location))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
